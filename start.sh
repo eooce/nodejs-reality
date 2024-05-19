46f91303a22d3b28b3d1beec5d82ef58626d51f2
eooce <142894633+eooce@users.noreply.github.com>
2024-05-19 19:19:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define Environment Variables
-[ -z "$UUID" ] && UUID=$(openssl rand -hex 16 | awk '{print substr($0,1,8)"-"substr($0,9,4)"-"substr($0,13,4)"-"substr($0,17,4)"-"substr($0,21,12)}')
-[ -z "$PORT" ] && PORT=$(shuf -i 2000-65000 -n 1)
-[ -z "$NEZHA_SERVER" ] && NEZHA_SERVER="nz.f4i.cn"
-[ -z "$NEZHA_PORT" ] && NEZHA_PORT="5555"
-[ -z "$NEZHA_KEY" ] && NEZHA_KEY=""
-[ -z "$FILE_PATH" ] && FILE_PATH="./app"
-[ -z "$SNI" ] && SNI="www.yahoo.com"
+export UUID=$(openssl rand -hex 16 | awk '{print substr($0,1,8)"-"substr($0,9,4)"-"substr($0,13,4)"-"substr($0,17,4)"-"substr($0,21,12)}')
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'} 
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
+export NEZHA_KEY=${NEZHA_KEY:-'abc123'}  
+export FILE_PATH=${FILE_PATH:-'./.npm'}
+export SNI=${SNI:-'www.yahoo.com'}
+export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 
 # Download Dependency Files
 ARCH=$(uname -m) && DOWNLOAD_DIR="${FILE_PATH}" && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
@@ -45,7 +45,7 @@ generate_config() {
 {
     "inbounds": [
         {
-            "port": $PORT,
+            "port": $SERVER_PORT,
             "protocol": "vless",
             "settings": {
                 "clients": [
@@ -95,7 +95,6 @@ generate_config
 # running files
 run() {
   if [ -e "${FILE_PATH}/npm" ]; then
-    chmod 777 "${FILE_PATH}/npm"
     tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
     if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
       NEZHA_TLS="--tls"
@@ -104,18 +103,17 @@ run() {
     fi
     if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
         nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-	sleep 2
-        pgrep -x "npm" > /dev/null && echo -e "\e[1;32mnpm is running\e[0m" || { echo -e "\e[1;35mnpm is not running, restarting...\e[0m"; pkill -x "npm" && nohup "${FILE_PATH}/npm" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32mnpm restarted\e[0m"; }
+	     sleep 2
+       echo -e "\e[1;32mnpm is running\e[0m"
     else
         echo -e "\e[1;35mNEZHA variable is empty,skiping runing\e[0m"
     fi
   fi
 
   if [ -e "${FILE_PATH}/web" ]; then
-    chmod 777 "${FILE_PATH}/web"
     nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
     sleep 2
-    pgrep -x "web" > /dev/null && echo -e "\e[1;32mweb is running\e[0m" || { echo -e "\e[1;35mweb is not running, restarting...\e[0m"; pkill -x "web" && nohup "${FILE_PATH}/web" -c ${FILE_PATH}/config.json >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32mweb restarted\e[0m"; }
+    echo -e "\e[1;32mweb is running\e[0m"
   fi
 
 }
@@ -129,12 +127,14 @@ ISP=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' |
 
 cat > ${FILE_PATH}/list.txt <<EOF
 
-vless://${UUID}@${IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#$ISP
+vless://${UUID}@${IP}:${SERVER_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#$ISP
 
 EOF
 cat ${FILE_PATH}/list.txt
 echo -e "\n\e[1;32m${FILE_PATH}/list.txt saved successfully\e[0m"
 echo ""
-echo -e "\n\e[1;32mInstall success!\e[0m"
+sleep 10 
+cleaar
+echo -e "\n\e[1;32mRunning success!\e[0m"
 
 exit 0
